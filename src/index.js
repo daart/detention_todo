1d4de736819c0aeff3dc141657378af445b9eb09
daart <artemko51@gmail.com>
2018-10-01 12:16:20 +0300

    pre mobx prep

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
+import { App } from './components/App';
+
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
 
-import { todos, visibilityFilter } from './App';
-
-const reducer = combineReducers({
-    todos,
-    visibilityFilter
-  }
-);
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
  document.getElementById('root'));
 registerServiceWorker();
